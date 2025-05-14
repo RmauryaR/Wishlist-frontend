fc47f867026f8794dd276de11d02d50cd916fb8d
RmauryaR <78673793+RmauryaR@users.noreply.github.com>
2025-05-14 17:13:46 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -24,7 +24,7 @@ export default function Signup() {
 
     try {
       const res = await axios.post(
-        "https://wishlist-backend-2-aoy9.onrender.com/api/auth/signup",
+        "https://wishlist-backend-u15l.onrender.com/api/auth/signup",
         formData
       );
       console.log(res.data);
